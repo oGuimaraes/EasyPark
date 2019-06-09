74ba6ade072efcbc8b268483aa5e0086606aebc7
oGuimaraes <otaviovgsr@gmail.com>
2019-06-09 19:08:01 -0300

    Estilização titulo entrada e saída veiculo

--- src/main/resources/static/javascript/entrada.js
+++ src/main/resources/static/javascript/entrada.js
@@ -1,5 +1,5 @@
-statusEntrada = document.getElementById("statusEntrada").innerHTML;
-if (statusEntrada != "Entrada de veículo não efetuada") {
+statusEntrada = document.getElementById("statusEntrada");
+if (statusEntrada.innerHTML != "Entrada de veículo não efetuada") {
     document.getElementById("veiculoSucesso").style.display = "block";
-
+    statusEntrada.style.color = "#9ecb4c";
 }
--- src/main/resources/static/javascript/saida.js
+++ src/main/resources/static/javascript/saida.js
@@ -1,4 +1,12 @@
-statusSaida = document.getElementById("statusSaida").innerHTML;
-if (statusSaida != "Saída não efetuada") {
+statusSaida = document.getElementById("statusSaida");
+if (statusSaida.innerHTML != "Saída não efetuada") {
     document.getElementById("veiculoSucesso").style.display = "block";
+    statusSaida.style.color = "#9ecb4c";
+}
+
+
+statusEntrada = document.getElementById("statusEntrada");
+if (statusEntrada.innerHTML != "Entrada de veículo não efetuada") {
+    document.getElementById("veiculoSucesso").style.display = "block";
+    statusEntrada.style.color = "#9ecb4c";
 }
